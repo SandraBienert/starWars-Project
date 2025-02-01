7e6e2032c750df4bb258b983686770106d2beff6
SandraBienert <sandrabienert@outlook.com>
2025-02-01 20:43:25 +0100

    configurando firebase

--- starWars-Project/starWars-Project/src/app/app.config.ts
+++ starWars-Project/starWars-Project/src/app/app.config.ts
@@ -1,12 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQsHsJQgT2UiqSxCE6h1vT9koTX9zTVEU",
+  authDomain: "starwars-firebase-auth.firebaseapp.com",
+  projectId: "starwars-firebase-auth",
+  storageBucket: "starwars-firebase-auth.firebasestorage.app",
+  messagingSenderId: "1041454265434",
+  appId: "1:1041454265434:web:5333fdb0e7bbacf2b1d463"
+};
+
+initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+
 ]
 };
+
+
+
--- starWars-Project/starWars-Project/src/app/app.routes.ts
+++ starWars-Project/starWars-Project/src/app/app.routes.ts
@@ -3,13 +3,24 @@ import { HomeComponent } from './components/home/home.component';
 import { StarshipDetailComponent } from './components/starship-detail/starship-detail.component';
 import { StarshipsListComponent } from './components/starships-list/starships-list.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { authGuard } from './guards/auth.guard';
+import { RegisterComponent } from './auth/register/register.component';
 
 
 export const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
+  { path: 'welcome',
+    canActivate:[authGuard],
+    loadComponent: () => import('./components/welcome/welcome.component')
+    .then(m => m.WelcomeComponent)
+  },
+  { path: 'register', loadComponent: () => import('./auth/register/register.component')
+    .then(m => m.RegisterComponent)
+  },
+  { path: 'login', loadComponent: () => import('./auth/login/login.component')
+    .then(m => m.LoginComponent)
+  },
   { path: 'starships', component: StarshipsListComponent },
   { path: 'home', component: HomeComponent }, // Define la ruta para 'home'
   { path: 'home/starship/:id', component: StarshipDetailComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home' }
+
 ];
