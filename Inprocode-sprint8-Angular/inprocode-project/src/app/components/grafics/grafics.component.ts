023a90f14ab21a847bda72a5de56ac97055a4ad3
SandraBienert <sandrabienert@outlook.com>
2025-02-04 11:26:39 +0100

    creacio de components necessaris, 1r commit

---
+++ Inprocode-sprint8-Angular/inprocode-project/src/app/components/grafics/grafics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafics',
+  imports: [],
+  templateUrl: './grafics.component.html',
+  styleUrl: './grafics.component.css'
+})
+export class GraficsComponent {
+
+}
