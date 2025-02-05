bc0f6684c8ec8ac51d3cb6b3e15cb7a8ddd10ac3
SandraBienert <sandrabienert@outlook.com>
2025-02-05 18:32:31 +0100

    desevolupat de mapa, falta configuracio

--- Inprocode-sprint8-Angular/inprocode-project/src/app/components/mapa/mapa.component.ts
+++ Inprocode-sprint8-Angular/inprocode-project/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import L from 'leaflet';
 
 @Component({
   selector: 'app-mapa',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './mapa.component.html',
   styleUrl: './mapa.component.css'
 })
-export class MapaComponent {
+export class MapaComponent implements OnInit {
+  private map: any;
+
+  ngOnInit(): void {
+    this.initMap();
 
 }
+private initMap(): void {
+  // Configura el mapa
+  this.map = L.map('map').setView([41.3851, 2.1734], 13); // Coordenades de Barcelona i zoom
+
+  // Afegeix una capa de tiles (fons del mapa)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(this.map);
+
+ // Afegeix un marcador
+  L.marker([41.3851, 2.1734])
+    .addTo(this.map)
+    .bindPopup('Barcelona')
+    .openPopup();
+}
+}
+
